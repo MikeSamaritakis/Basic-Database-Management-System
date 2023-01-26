04b93c4df931aaa6478257a8970f794a40b0967e
adony <adony101@gmail.com>
2023-01-26 18:32:27 +0200

    Ftiaksame e

--- src/java/Servlets/Servlet.java
+++ src/java/Servlets/Servlet.java
@@ -138,6 +138,8 @@ public class Servlet extends HttpServlet{
 
     }
 
+
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
