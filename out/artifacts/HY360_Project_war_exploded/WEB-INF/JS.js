4a860d056182445297f321acf827e46624437950
EMMANOUIL TZORBATZAKIS <csd4479@csd.uoc.gr>
2023-01-29 21:04:08 +0200

    payment history insert111

---
+++ out/artifacts/HY360_Project_war_exploded/WEB-INF/JS.js
@@ -0,0 +1,111 @@
+    function showPerm(){
+        hideTemp();
+        hideDel();
+        hideEdittemp();
+        hideEditperm();
+        hidepayment();
+        hideEditPayment()
+        document.getElementById("perm").style.display = "block";
+    }
+
+    function reloadpage(){
+        location.reload();
+    }
+
+    function hidePerm(){
+        document.getElementById("perm").style.display = "none";
+    }
+
+    function showTemp(){
+        hidePerm();
+        hideDel();
+        hideEdittemp();
+        hideEditperm();
+        hidepayment();
+        hideEditPayment()
+        document.getElementById("temp").style.display = "block";
+    }
+
+    function showEdittemp(){
+        hideTemp();
+        hidePerm();
+        hideDel();
+        hideEditperm();
+        hidepayment();
+        hideEditPayment()
+        document.getElementById("edittinfo").style.display = "block";
+    }
+
+    function  hideEdittemp(){
+        document.getElementById("edittinfo").style.display = "none";
+    }
+
+    function showEditperm(){
+        hideTemp();
+        hidePerm();
+        hideDel();
+        hideEdittemp();
+        hidepayment();
+        hideEditPayment()
+        document.getElementById("editpinfo").style.display = "block";
+    }
+
+    function  hideEditperm(){
+        document.getElementById("editpinfo").style.display = "none";
+    }
+
+    function showpayment(){
+        hideTemp();
+        hidePerm();
+        hideDel();
+        hideEdittemp();
+        hideEditperm();
+        hideEditPayment()
+        document.getElementById("payment").style.display = "block";
+    }
+
+    function  hidepayment(){
+        document.getElementById("payment").style.display = "none";
+    }
+
+    function hideTemp(){
+        document.getElementById("temp").style.display = "none";
+    }
+
+    function showDel(){
+        hideTemp();
+        hidePerm();
+        hideEdittemp();
+        hideEditperm();
+        hidepayment();
+        hideEditPayment()
+        document.getElementById("deleteemp").style.display = "block";
+    }
+
+    function hideDel(){
+        document.getElementById("deleteemp").style.display = "none";
+    }
+
+    function showEditPayment(){
+        hideDel();
+        hideTemp();
+        hidePerm();
+        hideEdittemp();
+        hideEditperm();
+        hidepayment();
+        document.getElementById(("EditPayment")).style.display = "block";
+    }
+
+    function hideEditPayment(){
+        document.getElementById(("EditPayment")).style.display = "none";
+    }
+
+    function hideAll(){
+        hideTemp();
+        hidePerm();
+        hideDel();
+        hideEdittemp();
+        hideEditperm();
+        hidepayment();
+        hideEditPayment()
+    }
